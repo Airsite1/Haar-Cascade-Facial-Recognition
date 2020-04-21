cacd3af72cbc0ea6f61a36e9f4052f9f2a7a9632
Sanjay S <srikumar.sanjay@gmail.com>
2020-04-20 23:09:29 -0400

    includes dependencies

--- face_rec.py
+++ face_rec.py
@@ -3,9 +3,9 @@ import numpy as np
 import cv2
 from PIL import Image
 
-face_classifier = cv2.CascadeClassifier('/Users/sanjay/Desktop/CODE/Python/Covid19-Face-Mask-Classifier-with-webscraped-images/haarcascade_frontalface_default.xml')
-eye_classifier = cv2.CascadeClassifier('/Users/sanjay/Desktop/CODE/Python/Covid19-Face-Mask-Classifier-with-webscraped-images/haarcascade_eye.xml')
-image = cv2.imread('/Users/sanjay/Desktop/IMG_3415.JPG')
+face_classifier = cv2.CascadeClassifier('[PATH TO haarcascade_frontalface_default.xml]'])
+eye_classifier = cv2.CascadeClassifier(['[PATH TO haarcascade_eye.xml]')
+image = cv2.imread('[PATH TO IMAGE]')
 gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
 
 faces = face_classifier.detectMultiScale(gray ,1.3 , 9) #location of different faces in file
@@ -16,6 +16,6 @@ if faces is ():
 for (x , y , w , h) in faces:
     roi_gray = gray[y:y+h  , x:x+w] #crop face out using indexing
     roi_color = image[y:y+h , x:x+w] #crop face similarily
-    cv2.imwrite("OH", roi_color)
-    print('ran')
+    cv2.imwrite("Cropped Face!", roi_color)
+    
 
